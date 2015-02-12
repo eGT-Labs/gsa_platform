f459fa7d3388d07a3abdbaec7b072b36cd7c9f0d
jaibapna <jaibapna>
2015-02-12 19:05:48 +0530

    Add recipes

---
+++ site_cookbooks/gsa/recipes/include_recipes.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: gsa
+# Recipe:: include_recipes
+#
+# Copyright 2015
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+include_recipe "utility::wget"
+include_recipe "utility::git"
+include_recipe "utility::java"
+
