432db59aed7aedd8356ec79624d623e4101fc812
Jai B <Jai B>
2015-03-13 17:41:33 +0530

    update

--- site_cookbooks/gsa/recipes/loadder.rb
+++ site_cookbooks/gsa/recipes/loadder.rb
@@ -9,7 +9,6 @@ $fbopen_uri="localhost:9200"
 
 $fbopen_index="fbopen"
 
-
 bash "reate an index on your Elasticsearch" do
 user "root"
 code <<-EOH
@@ -19,12 +18,13 @@ EOH
 only_if {::File.exists?("#{$apps_dir}/#{$git_repo_name}") }
 end
 
+
 bash "load fbo.gov loadder" do
 user "root"
 code <<-EOH
      cd #{$apps_dir}/#{$git_repo_name}/loaders/fbo.gov
 	  npm install
-     FBOPEN_ROOT=#{$apps_dir}/#{$git_repo_name} FBOPEN_URI=#{$fbopen_uri} FBOPEN_INDEX=#{$fbopen_index} ./fbo-nightly.sh
+     FBOPEN_ROOT=#{$apps_dir}/#{$git_repo_name} FBOPEN_URI=#{$fbopen_uri} FBOPEN_INDEX=#{$fbopen_index} ./fbo-nightly.sh | echo "already have"
 EOH
 only_if {::File.exists?("#{$apps_dir}/#{$git_repo_name}") }
 end
