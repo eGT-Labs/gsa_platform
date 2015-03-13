83bbe2e3ac98057daebc8999aec4c8b4a8c37c12
Jai B <Jai B>
2015-03-13 15:22:53 +0530

    nodejs recipe add

---
+++ site_cookbooks/gsa/recipes/nodejs.rb
@@ -0,0 +1,37 @@
+case node[:platform_family]
+
+		when "rhel"	
+
+			bash "Install Open Jdk" do
+			user "root"
+			code <<-EOH
+
+			    yum -y groupinstall "Development Tools" | echo "success"
+
+				cd /usr/src
+
+				wget http://nodejs.org/dist/v0.12.0/node-v0.12.0.tar.gz
+
+				tar xzvf node-v* && cd node-v*
+
+				./configure
+
+				make && make install
+
+
+			EOH
+			not_if {::File.exists?("/usr/local/bin/node") && `/usr/local/bin/node --version`.chomp == "v0.12.0" }
+			end
+
+
+
+
+		when "debian"
+
+
+
+else
+
+ raise '#{node[:platform_family]} not supported'
+
+end 
