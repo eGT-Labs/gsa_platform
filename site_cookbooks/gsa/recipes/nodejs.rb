d34f689a8bddbafc9937432a0dc971e6df2a9014
Jai B <Jai B>
2015-03-13 15:44:00 +0530

    process store logic add

--- site_cookbooks/gsa/recipes/nodejs.rb
+++ site_cookbooks/gsa/recipes/nodejs.rb
@@ -1,30 +1,20 @@
 case node[:platform_family]
-
 		when "rhel"	
-
 			bash "Install Open Jdk" do
 			user "root"
 			code <<-EOH
-
 			    yum -y groupinstall "Development Tools" | echo "success"
-
 				cd /usr/src
 
-				wget http://nodejs.org/dist/v0.10.9/node-v0.10.9.tar.gz
-
+				rm -rf node-v*
+				wget http://nodejs.org/dist/v0.12.0/node-v0.12.0.tar.gz
 				tar xzvf node-v* && cd node-v*
-
 				./configure
-
 				make && make install
 
-
 			EOH
-			not_if {::File.exists?("/usr/local/bin/node") && `/usr/local/bin/node --version`.chomp == "v0.10.9" }
+			not_if {::File.exists?("/usr/local/bin/node") && `/usr/local/bin/node --version`.chomp == "v0.12.0" }
 			end
-
-
-
 
 		when "debian"
 
